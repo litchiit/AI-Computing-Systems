087e25f6cc6ef0c39555087e14f860632b70c05c
litchiit <894085330@qq.com>
2021-01-02 20:52:53 +0800

    Update layers_2.py

--- exp_3_1_vgg/stu_upload/layers_2.py
+++ exp_3_1_vgg/stu_upload/layers_2.py
@@ -45,9 +45,15 @@ class ConvolutionalLayer(object):
         for idxn in range(self.input.shape[0]):
             for idxc in range(self.channel_out):
                 for idxh in range(height_out):
+                    h_start = idxh  * self.stride
+                    h_end = idxh * self.stride + self.kernel_size
                     for idxw in range(width_out):
                         # TODO: 计算卷积层的前向传播，特征图与卷积核的内积再加偏置
-                        self.output[idxn, idxc, idxh, idxw] = _______________________
+                        w_start = idxw  * self.stride
+                        w_end = idxw * self.stride + self.kernel_size
+                        T = self.weight[:, :, :, idxc]
+                        TT = self.input_pad[idxn, :, h_start:h_end, w_start:w_end]
+                        self.output[idxn, idxc, idxh, idxw] = np.sum(T*TT) + self.bias[idxc]
         return self.output
 
     def load_param(self, weight, bias):  # 参数加载
@@ -73,9 +79,13 @@ class MaxPoolingLayer(object):
         for idxn in range(self.input.shape[0]):
             for idxc in range(self.input.shape[1]):
                 for idxh in range(height_out):
+                    h_start = idxh * self.stride
+                    h_end = idxh * self.stride + self.kernel_size
                     for idxw in range(width_out):
                         # TODO： 计算最大池化层的前向传播， 取池化窗口内的最大值
-                        self.output[idxn, idxc, idxh, idxw] = _______________________
+                        w_start = idxw * self.stride
+                        w_end = idxw * self.stride + self.kernel_size
+                        self.output[idxn, idxc, idxh, idxw] = np.max(self.input[idxn, idxc, h_start:h_end, w_start:w_end])
         return self.output
 
 
